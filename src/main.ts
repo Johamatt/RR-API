f72e87b40e5d11b6536f3fb779b9faa9f8d1b284
johamatt94 <johamatt94@gmail.com>
2024-07-10 18:53:11 +0300

    added ssl cert

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as https from 'https';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +21,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
     }),
   );
+  // redirect https
+  const httpApp = express();
+  httpApp.get('*', (req, res) => {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  });
+  httpApp.listen(80);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
